92de28c9eb28aad486c46e2abdb32307139ae979
DamianZareba <79514948+DamianZareba@users.noreply.github.com>
2021-02-26 13:09:01 +0100

    połowa_pętle

---
+++ src/first_week/statements/lekcja_statements.java
@@ -0,0 +1,131 @@
+package first_week.statements;
+
+import java.util.Scanner;
+
+public class lekcja_statements {
+
+    /*
+    postać instruklcji warunkowej IF:
+
+    if(warunek){
+
+    zrobCos1;
+    zrobCos2;
+    ...
+    zrobCosX;
+    }
+    else{
+    zrobCosInnego1;
+    zrobCosInnego2;
+    ...
+    zrobCosInnegoX;
+    }
+
+        if(warunek){
+
+    zrobCos1;
+    zrobCos2;
+    ...
+    zrobCosX;
+    }
+
+        if(warunek){
+
+    zrobCos1;
+    zrobCos2;
+    ...
+    zrobCosX;
+    }
+        if(warunek){
+
+    zrobCos1;
+    zrobCos2;
+    ...
+    zrobCosX;
+    }
+     */
+
+    public static void main(String[] args) {
+
+        Scanner skaner = new Scanner(System.in);
+
+//        boolean stan = false;
+//        if (stan){
+//            System.out.println("Wykonałą się instrukcja IF.");
+//        }
+//        else{
+//            System.out.println("Wykonała się instrukcja Else. ");
+//        }
+//
+//        Scanner skaner = new Scanner(System.in);
+//        System.out.println("Podaj swój wiek: ");
+//        int wiek = skaner.nextInt();
+//
+//        if(wiek>= 18){
+//            System.out.println("Jesteś pełnoletni!");
+//        }
+//            else{
+//            System.out.println("Jesteś niepełnoletni!");
+//        }
+
+        System.out.println("Podaj liczbe: ");
+        int liczba = skaner.nextInt();
+
+//        if(10 < liczba && liczba < 20){
+//            System.out.println("Liczba mieści się w zakresie. ");
+//        }
+//        else{
+//            System.out.println("Liczba NIE mieści się w zakresie. ");
+//        }
+//
+//        System.out.println("Podaj liczbę całkowita A: ");
+//        int a = skaner.nextInt();
+//
+//        System.out.println("Podaj liczbę całkowita B: ");
+//        int b = skaner.nextInt();
+//
+//        if(a > b){
+//            System.out.println("A jest większe od B.");
+//        }
+//        if(a < b){
+//            System.out.println("A jest mniejsze od B.");
+//        }
+//        if(a == b){
+//            System.out.println("A jest równe B.");
+//        }
+//
+//        if(a > b){
+//            System.out.println("A jest większe od B.");
+//        }
+//        if(a < b){
+//            System.out.println("A jest mniejsze od B.");
+//        }
+//        if(a == b){
+//            System.out.println("A jest równe B.");
+//        }
+//        char charA = 'A';
+//        char charB = 'B';
+//
+//        if (charA > char B){
+//            System.out.println("charA ma większa wartość w tabeli ASCII.");
+//        }
+//        else{
+//            System.out.println("charA ma mniejszą warość od charB w tabeli ASCII.");
+//        }
+//
+//        String hello = "hello";
+//    }
+//}
+//        String world = "world";
+//
+//        if(hello.equals(world)){
+//            System.out.println("hello jest równe world");
+//        }
+//        else{
+//            System.out.println("hello nie jest równo world");
+
+        System.out.println();
+
+}
+
+
